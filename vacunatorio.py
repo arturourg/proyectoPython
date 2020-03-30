454083b68cb72be4174dfb8d5f6b520ba18b49f8
Kreysh <40643252+Kreysh@users.noreply.github.com>
2020-03-30 19:44:00 -0300

    Agregado Vacunar Botton y envio de variable

--- vacunatorio.py
+++ vacunatorio.py
@@ -17,7 +17,16 @@ mysql.connect_args["cursorclass"] = pymysql.cursors.DictCursor
 	
 @app.route('/')
 def hello():
-	return render_template('inicio.html')
+	url = "https://farmanet.minsal.cl/index.php/ws/getLocales"
+	r  =  requests.get(url)
+	diccionario = {}
+	for objeto in json.loads(r.text[1:]):
+		llave_region = f"region_{objeto['fk_region']}" 
+		if llave_region not in diccionario:
+			diccionario[llave_region] = []
+		if objeto["comuna_nombre"] not in diccionario[llave_region]:
+			diccionario[llave_region].append(objeto["comuna_nombre"])
+	return render_template('inicio.html', regiones=diccionario)
 
 
 
@@ -74,20 +83,27 @@ def paciente2():
 	
 	if request.method == "GET":
 		selec = request.args.get('selec', default = "", type = str)
-
+		vacunar = request.args.get('vacunar', default = "", type = str)
 
 	if request.method == "POST":
 		selec = request.form["selec"]
-
+		vacunar = request.form["vacunar"]
 
 	sql = "SELECT * FROM `paciente`"
 	cursor.execute(sql)
-	if selec!="":
+	if vacunar!="":
+		sql2="SELECT DISTINCT p.RUT,p.NOMBRE,p.APELLIDOS,p.FECHA_NACIMIENTO,v.NOMBRE_ENFERMEDAD,r.FECHA_VACUNA FROM paciente p,recibe r,vacuna v where p.RUT=r.RUT and r.NOMBRE_ENFERMEDAD=v.NOMBRE_ENFERMEDAD and p.RUT=%s"
+		cursor2.execute(sql2,(selec,))
+		rut_a_vacunar_ = cursor2.fetchall()
+		return render_template("vacunar_pacientes.html",rut_a_vacunar = rut_a_vacunar_)
+
+	elif selec!="":
 		sql2="SELECT p.RUT,p.NOMBRE,p.APELLIDOS,p.FECHA_NACIMIENTO,v.NOMBRE_ENFERMEDAD,r.FECHA_VACUNA FROM paciente p,recibe r,vacuna v where p.RUT=r.RUT and r.NOMBRE_ENFERMEDAD=v.NOMBRE_ENFERMEDAD and p.RUT=%s"
 		cursor2.execute(sql2,(selec,))
 		vacuna_del_paciente_ = cursor2.fetchall()
 		return render_template("mostrar_vacunas_paciente.html",vacuna_del_paciente = vacuna_del_paciente_) 
-
+	
+	
 
 	paciente_ = cursor.fetchall()
 	return render_template("paciente_vacuna.html",lista_paci = paciente_) 
@@ -122,104 +138,5 @@ def vacuna_paciente():
 	return render_template("vacuna_paciente.html",lista_vacu = vacuna_)
 
 
-#insertPaciente
-@app.route('/paciente/add', methods=["GET","POST"])
-def addPaciente():
-	cursor = mysql.get_db().cursor()
-
-	if request.method == "GET":
-		nombre = request.args.get('nombre', default = "", type = str)
-		apellidos = request.args.get('apellidos', default = "", type = str)
-		rut = request.args.get('rut', default = "", type = str)
-		nacimiento = request.args.get('nacimiento', default = "", type = str)
-
-		if nombre != "" and apellidos != "" and rut != "" and nacimiento !="":
-			try:
-				sql = "INSERT INTO PACIENTE (NOMBRE, APELLIDOS, RUT, FECHA_NACIMIENTO)"
-				sql+= " VALUES (%s,%s,%s,%s)"
-				cursor.execute(sql,(nombre,apellidos,rut, nacimiento))
-			except Exception as e:
-				print(e)
-
-	if request.method == "POST":
-		nombre = request.form['nombre']
-		apellidos =request.form['apellidos']
-		rut = request.form['rut']
-		nacimiento = request.form['nacimiento']
-
-		if nombre != "" and apellidos != "" and rut != "" and nacimiento !="":
-			try:
-				sql = "INSERT INTO PACIENTE (NOMBRE, APELLIDOS, RUT, FECHA_NACIMIENTO)"
-				sql+= " VALUES (%s,%s,%s,%s)"
-				cursor.execute(sql,(nombre, apellidos, rut, nacimiento))
-			except Exception as e:
-				print(e)
-	
-	return render_template('addPaciente.html', title='Registro de pacientes')
-
-#insertvacuna
-@app.route('/vacuna/add', methods=["GET","POST"])
-def addVacuna():
-	cursor = mysql.get_db().cursor()
-
-	if request.method == "GET":
-		enfermedad = request.args.get('enfermedad', default = "", type = str)
-
-		if enfermedad != "":
-			try:
-				sql = "INSERT INTO VACUNA (NOMBRE_ENFERMEDAD)"
-				sql+= " VALUES (%s)"
-				cursor.execute(sql,(enfermedad))
-			except Exception as e:
-				print(e)
-
-	if request.method == "POST":
-		enfermedad = request.form['enfermedad']
-
-		if enfermedad != "":
-			try:
-				sql = "INSERT INTO VACUNA (NOMBRE_ENFERMEDAD)"
-				sql+= " VALUES (%s)"
-				cursor.execute(sql,(enfermedad))
-			except Exception as e:
-				print(e)
-	
-	return render_template('addVacuna.html', title='Nueva vacuna')
-
-@app.route('/vacunar_paciente', methods=["GET","POST"])
-def vacunarPaciente():
-	cursor = mysql.get_db().cursor()
-	cursor2 = mysql.get_db().cursor()
-
-	if request.method == "GET":
-		selec = request.args.get('selec', default = "", type = str)
-
-	if request.method == "POST":
-		selec = request.form["selec"]
-
-
-	#if selec!="":	
-
-		#return render_template("mostrar_vacunas_paciente.html",vacuna_del_paciente = vacuna_del_paciente_) 
-	
-
-	sql="SELECT RUT, NOMBRE, APELLIDOS, FECHA_NACIMIENTO FROM paciente where  RUT=5476546484"
-	cursor.execute(sql)
-	vacuna_del_paciente_ = cursor.fetchall()
-
-
-	#MOSTRAR VACUNAS
-	sql2 = "SELECT NOMBRE_ENFERMEDAD FROM VACUNA"
-	cursor2.execute(sql2)
-	vacunas_ = cursor.fetchall()
-
-
-	return render_template("vacunar_paciente.html", paciente = vacuna_del_paciente_, lista_vacu = vacunas_)
-	#vacuna_del_paciente_ = cursor.fetchall()
-	#paciente_ = cursor.fetchall()
-	#return render_template("vacunar_paciente.html", paciente = vacuna_del_paciente_) #RUT=RUT, NOMBRE=NOMBRE, APELLIDOS=APELLIDOS,FECHA_NACIMIENTO=FECHA_NACIMIENTO paciente = paciente_,
-
-
 if __name__ == "__main__":
 	app.run(debug=True)
-
